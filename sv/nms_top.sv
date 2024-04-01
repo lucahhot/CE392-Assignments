45569b1afc64dc8fa6353830738e90fe73fa188c
Mitch Weng <tosaponweng@gmail.com>
2024-04-01 17:10:35 -0500

    nms testbench

---
+++ sv/nms_top.sv
@@ -0,0 +1,76 @@
+module nms_top #(
+    parameter WIDTH = 720,
+    parameter HEIGHT = 540,
+    parameter FIFO_BUFFER_SIZE = 8
+) (
+    input logic         clock,
+    input logic         reset,
+    output logic        image_full,
+    input logic         image_wr_en,
+    input logic [7:0]  image_din,
+
+    output logic        img_out_empty,
+    input logic         img_out_rd_en,
+    output logic [7:0]  img_out_dout
+);
+
+// Input wires
+
+logic [7:0]    image_dout;
+logic           image_empty;
+logic           image_rd_en;
+
+
+// Input wires to nms function
+logic           nms_wr_en;
+logic           nms_full;
+logic [7:0]     nms_out_din;
+
+
+// Output wires from nms function to output FIFO
+logic           img_out_wr_en;
+logic           img_out_full;
+logic [7:0]     img_out_din;
+
+fifo #(
+    .FIFO_BUFFER_SIZE(FIFO_BUFFER_SIZE),
+    .FIFO_DATA_WIDTH(8)
+) fifo_image_inst (
+    .reset(reset),
+    .wr_clk(clock),
+    .wr_en(image_wr_en),
+    .din(image_din),
+    .full(image_full),
+    .rd_clk(clock),
+    .rd_en(image_rd_en),
+    .dout(image_dout),
+    .empty(image_empty)
+);
+
+nms non_maximum_suppressor(
+    .clock(clock),
+    .reset(reset),
+    .in_rd_en(image_rd_en),
+    .in_empty(image_empty),
+    .in_dout(image_dout),
+    .out_wr_en(nms_wr_en),
+    .out_full(nms_full),
+    .out_din(nms_out_din)
+);
+
+fifo #(
+    .FIFO_BUFFER_SIZE(FIFO_BUFFER_SIZE),
+    .FIFO_DATA_WIDTH(8)
+) fifo_img_out_inst (
+    .reset(reset),
+    .wr_clk(clock),
+    .wr_en(nms_wr_en),
+    .din(nms_out_din),
+    .full(nms_full),
+    .rd_clk(clock),
+    .rd_en(img_out_rd_en),
+    .dout(img_out_dout),
+    .empty(img_out_empty)
+);
+
+endmodule
--- sv/non_maximum_suppresor.sv
+++ sv/non_maximum_suppresor.sv
@@ -19,6 +19,9 @@ parameter PIXEL_COUNT = WIDTH*HEIGHT;
 logic [7:0] shift_reg [SHIFT_REG_LEN-1:0];
 logic [7:0] shift_reg_c [SHIFT_REG_LEN-1:0];
 
+// output values
+logic [7:0] out, out_c;
+
 // Counters for prologue
 logic [$clog2(WIDTH+2)-1:0] counter, counter_c;
 
@@ -31,6 +34,23 @@ logic [$clog2(HEIGHT)-1:0] row, row_c;
 
 
 always_comb begin
+
+    // Keep shifting in values into the shift register until we reach the end of the image where we shift in zeros so that the
+    // sobel function can go through every single pixel
+    // Only shift a new value in if state is not in S2 (writing sobel value to FIFO)
+    if (state != S2) begin
+        if (in_empty == 1'b0) begin
+            // Implementing a shift right register
+            shift_reg_c[SHIFT_REG_LEN-2:0] = shift_reg[SHIFT_REG_LEN-1:1];
+            shift_reg_c[SHIFT_REG_LEN-1] = in_dout;
+            in_rd_en = 1'b1;
+        // If we have reached the end of the pixels from the FIFO, shift in zeros for padding
+        end else if ((row*HEIGHT) + col > (PIXEL_COUNT-1) - (WIDTH+2)) begin
+            shift_reg_c[SHIFT_REG_LEN-2:0] = shift_reg[SHIFT_REG_LEN-1:1];
+            shift_reg_c[SHIFT_REG_LEN-1] = 8'h00;
+        end
+    end
+
     case (state)
         S0: begin
             // Waiting for shift register to fill up enough to start sobel filter
@@ -50,7 +70,7 @@ always_comb begin
                 pixel2 = shift_reg[1];
                 pixel3 = shift_reg[2];
                 pixel4 = shift_reg[WIDTH];
-                pixel5 = shift_reg[WIDTH+1];
+                pixel5 = shift_reg[WIDTH+1]; // pixel being operated on
                 pixel6 = shift_reg[WIDTH+2];
                 pixel7 = shift_reg[WIDTH*2];
                 pixel8 = shift_reg[WIDTH*2+1];
@@ -61,15 +81,31 @@ always_comb begin
                 north_west = pixel1 + pixel9;
                 north_east = pixel3 + pixel7;
 
+                out_c = '0;
+
                 // if statements for non_maximum_suppressor
+                // consider if having multiple states instead of nested loops will impact performance
+                if (north_south >= east_west && north_south >= north_west && north_south >= north_east) begin
+                    if (pixel5 > pixel4 && pixel5 >= pixel6) begin
+                        out_c = pixel5;
+                    end
+                end else if (east_west >= north_west && east_west >= north_east) begin
+                    if (pixel5 > pixel2 && pixel5 >= pixel8) begin
+                        out_c = pixel5;
+                    end
+                end else if (north_west >= north_east) begin
+                    if (pixel5 > pixel3 && pixel5 >= pixel7) begin
+                        out_c = pixel5;
+                    end
+                end else begin
+                    if (pixel5 > pixel1 ** pixel5 >= pixel9) begin
+                        out_c = pixel5;
+                    end
+                end
 
 
-                // Using the absolute value
-                cx_c = ($signed(cx_c) < 0) ? -cx_c : cx_c;
-                cy_c = ($signed(cy_c) < 0) ? -cy_c : cy_c;
             end else begin
-                cx_c = '0;
-                cy_c = '0;
+                out_c = '0;
             end
             // Increment col and row trackers
             if (col == WIDTH - 1) begin
@@ -79,6 +115,26 @@ always_comb begin
                 col_c++;
 
             next_state = S2;
+        end
+
+        S2: begin
+            if (out_full == 1'b0) begin
+                out_din = out;
+                out_wr_en = 1'b1;
+                next_state = S1;
+            end
+        end
+
+        default: begin
+            next_state = S0;
+            in_rd_en = 1'b0;
+            out_wr_en = 1'b0;
+            out_din = 8'h00;
+            counter_c = 'X;
+            col_c = 'X;
+            row_c = 'X;
+            out_c = 'X;
+            shift_reg_c = '{default: '{default: '0}};
         end
     endcase
 
