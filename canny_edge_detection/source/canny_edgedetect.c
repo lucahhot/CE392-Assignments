cec8f7aaef383030459fca60b5c9990fc375111f
Zhehao Zhu <uko9054@moore.wot.eecs.northwestern.edu>
2024-04-04 21:33:32 -0500

    append hough transform and highlighting

--- canny_edge_detection/source/canny_edgedetect.c
+++ canny_edge_detection/source/canny_edgedetect.c
@@ -2,10 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
+#include <math.h>
 
 #define high_threshold 48
 #define low_threshold 12
-	
+#define Theta_range 180
+#define PI 3.1415926
+#define HOUGH_TRANSFORM_THRESHOLD 200
+#define LINE_LENGTH 3000
+
 
 struct pixel {
    unsigned char b;
@@ -271,6 +276,101 @@ void hysteresis_filter(unsigned char *in_data, int height, int width, unsigned c
 	}
 }
 
+void draw_a_line(int height, int width, int rho, double sin_theta, double cos_theta, unsigned char *edge, struct pixel* origin_image){
+   // printf("starting a line\n");
+   double x0 = rho * cos_theta;
+   double y0 = rho * sin_theta;
+   for(int k=-LINE_LENGTH; k<LINE_LENGTH; k++){
+      int x1 = (int)(x0 + k*(-sin_theta));
+      int y1 = (int)(y0 + k *(cos_theta));
+
+      // int index = y1*width + x1;
+      if(x1>=0 && x1<width && y1>=0 && y1<height) {
+         // printf("%d, %d, %d\n", x1, y1, y1*width + x1);
+         origin_image[y1*width + x1] = (struct pixel){0, 255, 0};
+      }
+      // if(x1>=0 && x1<width - 1 && y1>=0 && y1<height) {origin_image[y1*width + x1 +1 ] = (struct pixel){0, 255, 0};}
+   }
+}
+
+
+void hough_transform(int height, int width, unsigned char *edge, struct pixel* origin_image){
+
+   //pre-calculate the sin and cos
+   double theta_base = -90.;
+   double sin_theta[Theta_range];
+   double cos_theta[Theta_range];
+   for(int i=0; i<Theta_range; i++){
+      double temp = (double)i + theta_base;
+      double temp_radians = temp * PI /180;
+      sin_theta[i] = sin(temp_radians);
+      cos_theta[i] = cos(temp_radians);
+   }
+
+   // printf("precalculation finished\n");
+
+   //init the accumulator
+   int rho_max = (int)sqrt(height*height + width * width);
+   int **accumulator = (int **)malloc(2*rho_max*sizeof(int*));
+   for(int i=0; i<2*rho_max; i++){
+      accumulator[i] = (int *)malloc(Theta_range*sizeof(int));
+   }
+   for(int i=0; i<2*rho_max; i++){
+      for(int j=0; j<Theta_range; j++){
+         accumulator[i][j] = 0;
+      }
+   }
+   printf("init accumulator finished\n");
+
+   //suppose the edges are white
+   //process the accumulator
+   for(int y=0; y<height; y++){
+      for(int x = 0; x<width; x++){
+         if(edge[y*width + x] > 0){
+            for(int k=0; k<Theta_range; k++){
+               int rho = (int)(x*cos_theta[k] + y*sin_theta[k]) + rho_max;
+               accumulator[rho][k] += 1;
+            }
+         }
+      }
+   }
+
+   printf("accumulate finished\n");
+
+   //fliter the accumulator and draw lines
+   for(int i = 0; i<2*rho_max; i++){
+      for(int j=0; j<Theta_range; j++){
+         if(accumulator[i][j]>=HOUGH_TRANSFORM_THRESHOLD){
+            draw_a_line(height, width, i-rho_max, sin_theta[j], cos_theta[j], edge, origin_image);
+            // return;
+         }
+      }
+   }
+
+   printf("draw line finished\n");
+
+   //free the accumulator
+   for(int i=0; i<2*rho_max; i++){
+      free(accumulator[i]);
+   }
+   free(accumulator);
+}
+
+void write_bmp(const char *filename, unsigned char* header, struct pixel* data) 
+{
+   FILE* file = fopen(filename, "wb");
+
+   // get height and width of image
+   int width = (int)(header[19] << 8) | header[18];
+   int height = (int)(header[23] << 8) | header[22];
+   int size = width * height;
+   
+   // write the 54-byte header
+   fwrite(header, sizeof(unsigned char), 54, file); 
+   fwrite(data, sizeof(struct pixel), size, file); 
+   
+   fclose(file);
+}
 
 int main(int argc, char *argv[]) {
 	struct pixel *rgb_data = (struct pixel *)malloc(720*540*sizeof(struct pixel));
@@ -313,6 +413,9 @@ int main(int argc, char *argv[]) {
 	/// Hysteresis
 	hysteresis_filter(nms_data, height, width, h_data);
 	write_grayscale_bmp("../images/stage4_hysteresis.bmp", header, h_data);
+
+   hough_transform(height, width, h_data, rgb_data);
+   write_bmp("../images/output.bmp", header, rgb_data);
 
 	return 0;
 }
