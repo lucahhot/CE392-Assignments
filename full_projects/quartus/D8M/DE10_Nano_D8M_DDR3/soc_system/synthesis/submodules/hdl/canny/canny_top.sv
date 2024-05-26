b8a5ecdbdfe4d3478148e0bea2540971f38ff913
Luc Ah-Hot <luc.ahhot@gmail.com>
2024-05-25 23:52:27 -0500

    canny without gaussian 30fps

--- full_projects/quartus/D8M/DE10_Nano_D8M_DDR3/soc_system/synthesis/submodules/hdl/canny/canny_top.sv
+++ full_projects/quartus/D8M/DE10_Nano_D8M_DDR3/soc_system/synthesis/submodules/hdl/canny/canny_top.sv
@@ -104,135 +104,135 @@ fifo #(
     .din(gaussian_din),
     .full(gaussian_full),
     .rd_clk(clock),
-    .rd_en(gaussian_rd_en),
-    .dout(gaussian_dout),
-    .empty(gaussian_empty)
+    .rd_en(sobel_rd_en),
+    .dout(sobel_dout),
+    .empty(sobel_empty)
     // .rd_en(img_out_rd_en),
     // .dout(img_out_dout),
     // .empty(img_out_empty)
 );
 
-gaussian_blur #(
+// gaussian_blur #(
+//     .WIDTH(WIDTH),
+//     .HEIGHT(HEIGHT)
+// ) gaussian_inst(
+//     .clock(clock),
+//     .reset(reset),
+//     .in_rd_en(gaussian_rd_en),
+//     .in_empty(gaussian_empty),
+//     .in_dout(gaussian_dout),
+//     .out_wr_en(sobel_wr_en),
+//     .out_full(sobel_full),
+//     .out_din(sobel_din)
+// );
+
+// fifo #(
+//     .FIFO_DATA_WIDTH(8),
+//     .FIFO_BUFFER_SIZE(FIFO_BUFFER_SIZE)
+// ) fifo_sobel_inst (
+//     .reset(reset),
+//     .wr_clk(clock),
+//     .wr_en(sobel_wr_en),
+//     .din(sobel_din),
+//     .full(sobel_full),
+//     .rd_clk(clock),
+//     // .rd_en(sobel_rd_en),
+//     // .dout(sobel_dout),
+//     // .empty(sobel_empty)
+//     .rd_en(img_out_rd_en),
+//     .dout(img_out_dout),
+//     .empty(img_out_empty)
+// );
+
+sobel #(
     .WIDTH(WIDTH),
     .HEIGHT(HEIGHT)
-) gaussian_inst(
+) sobel_inst(
     .clock(clock),
     .reset(reset),
-    .in_rd_en(gaussian_rd_en),
-    .in_empty(gaussian_empty),
-    .in_dout(gaussian_dout),
-    .out_wr_en(sobel_wr_en),
-    .out_full(sobel_full),
-    .out_din(sobel_din)
+    .in_rd_en(sobel_rd_en),
+    .in_empty(sobel_empty),
+    .in_dout(sobel_dout),
+    .out_wr_en(nms_wr_en),
+    .out_full(nms_full),
+    .out_din(nms_din)
 );
 
 fifo #(
     .FIFO_DATA_WIDTH(8),
     .FIFO_BUFFER_SIZE(FIFO_BUFFER_SIZE)
-) fifo_sobel_inst (
+) fifo_nms_inst (
     .reset(reset),
     .wr_clk(clock),
-    .wr_en(sobel_wr_en),
-    .din(sobel_din),
-    .full(sobel_full),
+    .wr_en(nms_wr_en),
+    .din(nms_din),
+    .full(nms_full),
     .rd_clk(clock),
-    // .rd_en(sobel_rd_en),
-    // .dout(sobel_dout),
-    // .empty(sobel_empty)
+    .rd_en(nms_rd_en),
+    .dout(nms_dout),
+    .empty(nms_empty)
+    // .rd_en(img_out_rd_en),
+    // .dout(img_out_dout),
+    // .empty(img_out_empty)
+);
+
+non_maximum_suppressor #(
+    .WIDTH(WIDTH),
+    .HEIGHT(HEIGHT)
+) nms_inst(
+    .clock(clock),
+    .reset(reset),
+    .in_rd_en(nms_rd_en),
+    .in_empty(nms_empty),
+    .in_dout(nms_dout),
+    .out_wr_en(hysteresis_wr_en),
+    .out_full(hysteresis_full),
+    .out_din(hysteresis_din)
+);
+
+fifo #(
+    .FIFO_DATA_WIDTH(8),
+    .FIFO_BUFFER_SIZE(FIFO_BUFFER_SIZE)
+) fifo_hysteresis_inst (
+    .reset(reset),
+    .wr_clk(clock),
+    .wr_en(hysteresis_wr_en),
+    .din(hysteresis_din),
+    .full(hysteresis_full),
+    .rd_clk(clock),
+    .rd_en(hysteresis_rd_en),
+    .dout(hysteresis_dout),
+    .empty(hysteresis_empty)
+);
+
+hysteresis #(
+    .WIDTH(WIDTH),
+    .HEIGHT(HEIGHT)
+) hysteresis_inst (
+    .clock(clock),
+    .reset(reset),
+    .in_rd_en(hysteresis_rd_en),
+    .in_empty(hysteresis_empty),
+    .in_dout(hysteresis_dout),
+    .out_wr_en(img_out_wr_en),
+    .out_full(img_out_full),
+    .out_din(img_out_din)
+);
+
+fifo #(
+    .FIFO_DATA_WIDTH(8),
+    .FIFO_BUFFER_SIZE(FIFO_BUFFER_SIZE)
+) fifo_img_out_inst (
+    .reset(reset),
+    .wr_clk(clock),
+    .wr_en(img_out_wr_en),
+    .din(img_out_din),
+    .full(img_out_full),
+    .rd_clk(clock),
     .rd_en(img_out_rd_en),
     .dout(img_out_dout),
     .empty(img_out_empty)
 );
-
-// sobel #(
-//     .WIDTH(WIDTH),
-//     .HEIGHT(HEIGHT)
-// ) sobel_inst(
-//     .clock(clock),
-//     .reset(reset),
-//     .in_rd_en(sobel_rd_en),
-//     .in_empty(sobel_empty),
-//     .in_dout(sobel_dout),
-//     .out_wr_en(nms_wr_en),
-//     .out_full(nms_full),
-//     .out_din(nms_din)
-// );
-
-// fifo #(
-//     .FIFO_DATA_WIDTH(8),
-//     .FIFO_BUFFER_SIZE(FIFO_BUFFER_SIZE)
-// ) fifo_nms_inst (
-//     .reset(reset),
-//     .wr_clk(clock),
-//     .wr_en(nms_wr_en),
-//     .din(nms_din),
-//     .full(nms_full),
-//     .rd_clk(clock),
-//     // .rd_en(nms_rd_en),
-//     // .dout(nms_dout),
-//     // .empty(nms_empty)
-//     .rd_en(img_out_rd_en),
-//     .dout(img_out_dout),
-//     .empty(img_out_empty)
-// );
-
-// non_maximum_suppressor #(
-//     .WIDTH(WIDTH),
-//     .HEIGHT(HEIGHT)
-// ) nms_inst(
-//     .clock(clock),
-//     .reset(reset),
-//     .in_rd_en(nms_rd_en),
-//     .in_empty(nms_empty),
-//     .in_dout(nms_dout),
-//     .out_wr_en(hysteresis_wr_en),
-//     .out_full(hysteresis_full),
-//     .out_din(hysteresis_din)
-// );
-
-// fifo #(
-//     .FIFO_DATA_WIDTH(8),
-//     .FIFO_BUFFER_SIZE(FIFO_BUFFER_SIZE)
-// ) fifo_hysteresis_inst (
-//     .reset(reset),
-//     .wr_clk(clock),
-//     .wr_en(hysteresis_wr_en),
-//     .din(hysteresis_din),
-//     .full(hysteresis_full),
-//     .rd_clk(clock),
-//     .rd_en(hysteresis_rd_en),
-//     .dout(hysteresis_dout),
-//     .empty(hysteresis_empty)
-// );
-
-// hysteresis #(
-//     .WIDTH(WIDTH),
-//     .HEIGHT(HEIGHT)
-// ) hysteresis_inst (
-//     .clock(clock),
-//     .reset(reset),
-//     .in_rd_en(hysteresis_rd_en),
-//     .in_empty(hysteresis_empty),
-//     .in_dout(hysteresis_dout),
-//     .out_wr_en(img_out_wr_en),
-//     .out_full(img_out_full),
-//     .out_din(img_out_din)
-// );
-
-// fifo #(
-//     .FIFO_DATA_WIDTH(8),
-//     .FIFO_BUFFER_SIZE(FIFO_BUFFER_SIZE)
-// ) fifo_img_out_inst (
-//     .reset(reset),
-//     .wr_clk(clock),
-//     .wr_en(img_out_wr_en),
-//     .din(img_out_din),
-//     .full(img_out_full),
-//     .rd_clk(clock),
-//     .rd_en(img_out_rd_en),
-//     .dout(img_out_dout),
-//     .empty(img_out_empty)
-// );
 
 
 
